e714a21fe674b04f9f142cb964c939c1d58c8638
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-19 13:12:06 +0300

    Lists water asset page in routes

---
+++ frontend/src/components/AssetForm.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+const initialState = {
+    name: "",
+    asset_type: "",
+    installation_date: "",
+    material: "",
+    capacity: "",
+    location: "",
+    latitude: "",
+    longitude: "",
+    status: "",
+    last_maintenance: ""
+}
+
+function AssetForm({asset, onSubmit}){
+
+}
+
+
+export default AssetForm
