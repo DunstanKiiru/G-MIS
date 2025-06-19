edc7d84ed935a4dc0c97a6f9a03fde36689365ce
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-19 13:38:51 +0300

    installs bootstrap and also adding Error page to routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import WaterAssets from "./pages/WaterAssets"
+import Error from './pages/Error'
 
 const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
     {
         path: '/waterassets',
         element: <WaterAssets />
+    },
+    {
+        path: '*',
+        element: <Error />
     }
 ])
 
