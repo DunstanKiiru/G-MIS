190deaa793e7c35c97ebb721c8098bf67adf3e9d
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-19 15:56:55 +0300

    Styles the Header component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 import "./Header.css";
 
 const Header = () => {
   return (
     <section className="h-wrapper">
-      <div className="Columncenter  h-container">
-        <img src="/garissa.png" alt="MIS logo" width={100} />
+      <div className="h-container">
+        <NavLink to="/">
+          <img src="/garissa.png" alt="MIS logo" width={100} className="logo" />
+        </NavLink>
       </div>
 
-      <div className="ColumnCenter h-menu">
-        <Link to="/">Home</Link>
-        <Link to="/waterassets">Assets</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/login" className="btn btn-outline-primary mt-2">
+      <div className="h-menu">
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/waterassets">Assets</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/login" className="btn btn-outline-primary mt-2">
           Login
-        </Link>
+        </NavLink>
       </div>
     </section>
   );
