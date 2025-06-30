7256866bb0d40b4dd2e4772b7326165d93ccad61
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-30 03:10:10 +0300

    registers routes in __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from app.modules.water.routes import api
 import os
 from dotenv import load_dotenv
 from app.modules.community_contributions.routes import bp as community_bp
-from app.modules.om_visit.routes import bp as om_visits_bp
+from app.modules.om_visits.routes import bp as om_visits_bp
 from app.modules.water_quality_tests.routes import bp as quality_bp
 from app.modules.staff_development.routes import bp as staff_dev_bp
 
@@ -22,5 +22,10 @@ def create_app():
     migrate.init_app(app, db)
 
     app.register_blueprint(api)
+    app.register_blueprint(community_bp)
+    app.register_blueprint(om_visits_bp)
+    app.register_blueprint(quality_bp)
+    app.register_blueprint(staff_dev_bp)
+    app.register_blueprint(spare_parts_bp)
 
     return app
